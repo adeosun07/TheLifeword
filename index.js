1af989a96b745dfbe262dade0ef45088c1ad1f80
Adeosun Oluwatimileyin <oluwatimileyinadeosun@gmil.com>
2025-07-08 15:51:47 -0700

    update the database configuration

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ const postsFile = path.join(__dirname, 'data', 'posts.json');
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+export default pool;
 
 
 app.use(bodyParser.urlencoded({extended: true}));
