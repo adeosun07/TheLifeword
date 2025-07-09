77bc3275aedebc8003a9ba48e2410801696f7557
Adeosun Oluwatimileyin <oluwatimileyinadeosun@gmil.com>
2025-07-09 02:05:23 -0700

    update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import bcrypt from 'bcrypt';
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const saltRounds = 10;
